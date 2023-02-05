46f1aa6191f3686f9bb1f28422267157127d3c22
Artem Daugel-Dauge <daugeldauge@gmail.com>
2023-02-05 22:24:20 +0100

    Bump Kotlin & KSP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,10 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     versionCatalogs {
         create("libs") {
-            val kotlinVersion = "1.8.0-RC"
+            val kotlinVersion = "1.8.0"
             version("kotlin", kotlinVersion)
 
-            val kspVersion = "$kotlinVersion-1.0.8"
+            val kspVersion = "$kotlinVersion-1.0.9"
 
             library("kotlinpoet", "com.squareup:kotlinpoet:1.9.0")
             library("compileTestingKsp", "com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.4")
